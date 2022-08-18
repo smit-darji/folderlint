d753bd0a74fdff945a8b2ada22a7fa3d2f5a7326
YMakwanaIH <yogesh.makwana@invitationhomes.com>
2022-08-18 16:13:41 +0530

    ERSrdtrfiytswgb

--- public/file_name_and_directory_name_validator.sh
+++ public/file_name_and_directory_name_validator.sh
@@ -1,5 +1,9 @@
 echo "${CHANGED_FILES}"
-
+echo 
+# for str in ${CHANGED_FILES[@]}; do
+#   echo $str
+# done
+# mapfile -t changedfiles < <("${CHANGED_FILES}")
 arr=("")
 arr+=("${CHANGED_FILES}")
 echo ${arr[@]}
@@ -11,6 +15,8 @@ file_names_to_ignore=("changelog.xml", "pom.xml", "ReadMe.md")
 for i in "${!arr[@]}"; do
     if [[ "${arr[i]}" == .github* ]]; then
         unset 'arr[i]'
+        echo "Failed To Remove files of .github directory from list"
+        exit 1
     fi
 done
 
@@ -20,11 +26,15 @@ unique_file_names=()
 for i in "${!arr[@]}"; do
     if [[ ! " ${file_names_to_ignore[*]} " =~ " ${arr[i]##*/} " ]]; then
         unique_file_names+=(${arr[i]##*/})
+        echo "Invalid Unique File Name ${arr[i]##*/})"
+        exit 1
     fi
     IFS='/' read -ra path <<< "${arr[i]%/*}/"
     for i in "${path[@]}"; do
         if [[ ! " ${unique_dirs[*]} " =~ " ${i} " ]]; then
             unique_dirs+=(${i})
+            echo "Invalid Unique Directory Name ${i})"
+            exit 1
         fi
     done
 done
@@ -34,7 +44,8 @@ invalid_dirs=()
 for dir in "${unique_dirs[@]}"; do
     if [[ ! "${dir}" =~ ^[A-Z0-9._]*$ ]]; then
         invalid_dirs+=(${dir}) 
-        
+        echo "Invalid Directory Name ${dir})"
+        exit 1
     fi
 done
 
@@ -43,8 +54,8 @@ invalid_file_names=()
 for file_name in "${unique_file_names[@]}"; do
     if [[ ! "${file_name}" =~ [0-9]{4}_[A-Z0-9_]*.[a-zA-Z]*$ ]]; then
         invalid_file_names+=(${file_name})
-        echo "Invalid FileName"
-        
+        echo "Invalid FIleName ${file_name})"
+        exit 1
     fi
 done
 
@@ -53,11 +64,13 @@ if [[ ! -z "$invalid_dirs" || ! -z "$invalid_file_names" ]];
         echo "Failed!!"
         if [[ ! -z "$invalid_dirs" ]]; then
             echo "${invalid_dirs[@]}"
-            
+            echo "Invalid Directory ${invalid_dirs[@]} "
+            exit 1
         fi 
         if [[ ! -z "$invalid_file_names" ]]; then
             echo "${invalid_file_names[@]}"
-            
+            echo "Invalid FileName ${invalid_dirs[@]} "
+            exit 1
         fi
     else
         echo "Success!!"
