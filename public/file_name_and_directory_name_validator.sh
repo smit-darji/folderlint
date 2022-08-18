4d27b741e6f4e21cb640d5bea8748e992492ad72
YMakwanaIH <yogesh.makwana@invitationhomes.com>
2022-08-18 16:33:02 +0530

    ERSrdtrfiytswgb

--- public/file_name_and_directory_name_validator.sh
+++ public/file_name_and_directory_name_validator.sh
@@ -33,7 +33,7 @@ invalid_dirs=()
 for dir in "${unique_dirs[@]}"; do
     if [[ ! "${dir}" =~ ^[A-Z0-9._]*$ ]]; then
         invalid_dirs+=(${dir}) 
-        exit 1
+        exit 0
     fi
 done
 
@@ -43,7 +43,7 @@ for file_name in "${unique_file_names[@]}"; do
     if [[ ! "${file_name}" =~ [0-9]{4}_[A-Z0-9_]*.[a-zA-Z]*$ ]]; then
         invalid_file_names+=(${file_name})
         echo "Invalid FileName"
-        exit 1
+        exit 0
     fi
 done
 
@@ -52,11 +52,11 @@ if [[ ! -z "$invalid_dirs" || ! -z "$invalid_file_names" ]];
         echo "Failed!!"
         if [[ ! -z "$invalid_dirs" ]]; then
             echo "${invalid_dirs[@]}"
-            exit 1
+            exit 0
         fi 
         if [[ ! -z "$invalid_file_names" ]]; then
             echo "${invalid_file_names[@]}"
-            exit 1
+            exit 0
         fi
     else
         echo "Success!!"
