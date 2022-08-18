f2a6aaf49933bd41f3d69cacf7e78d073aa06fd8
YMakwanaIH <yogesh.makwana@invitationhomes.com>
2022-08-18 11:25:27 +0530

    ERSrdtrfiytswgb

--- public/file_name_and_directory_name_validator.sh
+++ public/file_name_and_directory_name_validator.sh
@@ -14,7 +14,6 @@ file_names_to_ignore=("changelog.xml", "pom.xml", "ReadMe.md")
 
 # Remove files of .github directory from list
 for i in "${!arr[@]}"; do
-    echo "Yout are in ignore file"
     if [[ "${arr[i]}" == .github* ]]; then
         unset 'arr[i]'
     fi
@@ -25,12 +24,10 @@ unique_dirs=()
 unique_file_names=()
 for i in "${!arr[@]}"; do
     if [[ ! " ${file_names_to_ignore[*]} " =~ " ${arr[i]##*/} " ]]; then
-        echo "Yout are in unique dir"
         unique_file_names+=(${arr[i]##*/})
     fi
     IFS='/' read -ra path <<< "${arr[i]%/*}/"
     for i in "${path[@]}"; do
-        echo "Yout are in split"
         if [[ ! " ${unique_dirs[*]} " =~ " ${i} " ]]; then
             unique_dirs+=(${i})
         fi
@@ -40,18 +37,18 @@ done
 # Get Invalid Directory names
 invalid_dirs=()
 for dir in "${unique_dirs[@]}"; do
-    echo "Yout are in  dir name"
     if [[ ! "${dir}" =~ ^[A-Z0-9._]*$ ]]; then
         invalid_dirs+=(${dir}) 
+        exit 0
     fi
 done
 
 # Get Invalid file names
 invalid_file_names=()
 for file_name in "${unique_file_names[@]}"; do
-    echo "Yout are in invalid filename"
     if [[ ! "${file_name}" =~ [0-9]{4}_[A-Z0-9_]*.[a-zA-Z]*$ ]]; then
-        invalid_file_names+=(${file_name}) 
+        invalid_file_names+=(${file_name})
+        exit 0
     fi
 done
 
@@ -59,12 +56,12 @@ if [[ ! -z "$invalid_dirs" || ! -z "$invalid_file_names" ]];
     then
         echo "Failed!!"
         if [[ ! -z "$invalid_dirs" ]]; then
-            echo "Invalid Directory Names"
             echo "${invalid_dirs[@]}"
+            exit 0
         fi 
         if [[ ! -z "$invalid_file_names" ]]; then
-            echo "Invalid File Names"
             echo "${invalid_file_names[@]}"
+            exit 0
         fi
     else
         echo "Success!!"
